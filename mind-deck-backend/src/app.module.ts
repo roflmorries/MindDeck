baf4957f22e4526c0fb8ef1dfbc6a1a5af3ed4cb
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-10-28 17:53:23 +0200

    update modules

--- mind-deck-backend/src/app.module.ts
+++ mind-deck-backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [ConfigModule.forRoot() ,AuthModule, UserModule],
 })
 export class AppModule {}
