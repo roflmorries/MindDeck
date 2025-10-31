cd119b305bbd1b74a8f8813f49703b3281f6ce2b
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-10-31 16:26:00 +0200

    feat(user) create user service logic

--- mind-deck-backend/src/user/user.service.ts
+++ mind-deck-backend/src/user/user.service.ts
@@ -1,20 +1,112 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from '../prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
-  getById(id: string) {
+  async findByMail(email: string): Promise<User | null> {
     return this.prisma.user.findUnique({
-      where: {
-        id
-      },
-      include : {
-        habits: true
+      where: { email },
+      include: {
+        socialAccounts: true
       }
+    })
+  }
+
+  async findUserById(id: string): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: { id },
+      include: {
+        socialAccounts: true
+      }
+    })
+  }
+
+  async findByMagicLinkToken(token: string): Promise<User | null> {
+    return this.prisma.user.findFirst({
+      where: {
+        magicLinkToken: token,
+        magicLinkExpires: {
+          gt: new Date()
+        }
+      }
+    })
+  }
+
+  async create(data: CreateUserDto): Promise<User> {
+    return this.prisma.user.create({
+      data,
+      include: {
+        socialAccounts: true
+      },
+    });
+  }
+
+  async update(id: string, data: UpdateUserDto) : Promise<User> {
+    return this.prisma.user.update({
+      where: {id},
+      data,
+      include: {
+        socialAccounts: true
+      },
+    });
+  }
+
+  async updateMagicLinkToken(id: string, token: string, expires: Date): Promise<User> {
+    return this.prisma.user.update({
+      where: {id},
+      data: {
+        magicLinkToken: token,
+        magicLinkExpires: expires
+      }
+    })
+  }
+
+  async clearMagicLinkToken(id: string) : Promise<User> {
+    return this.prisma.user.update({
+      where: {id},
+      data: {
+        magicLinkToken: null,
+        magicLinkExpires: null,
+        isEmailVerified: true
+      },
+    });
+  }
+
+  async verifyEmail(id: string): Promise<User> {
+    return this.prisma.user.update({
+      where: {id},
+      data: {
+        isEmailVerified: true,
+        emailVerifiedToken: null,
+        emailVerificationExpires: null
+      }
+    })
+  }
+
+  async updateEmailVerificationToken(id: string, token: string, expires: Date): Promise<User> {
+    return this.prisma.user.update({
+      where: {id},
+      data: {
+        emailVerifiedToken: token,
+        emailVerificationExpires: expires
+      }
+    })
+  }
+
+  async remove(id: string): Promise<User> {
+    const user = await this.findUserById(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found')
+    }
+
+    return this.prisma.user.delete({
+      where: {id}
     })
   }
 }
