b545d86c58280cfad1938bf5fc162c46822bdf37
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-10-31 16:25:02 +0200

    feat(user) create user DTO

--- mind-deck-backend/src/user/dto/create-user.dto.ts
+++ mind-deck-backend/src/user/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { IsBoolean, IsEmail, IsOptional, IsString } from "class-validator";
+
+export class CreateUserDto {
+
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  name?: string
+
+  @IsOptional()
+  @IsString()
+  passwordHash?: string
+
+  @IsOptional()
+  @IsString()
+  avatar?: string
+
+  @IsOptional()
+  @IsBoolean()
+  isEmailVerified?: boolean
+}
