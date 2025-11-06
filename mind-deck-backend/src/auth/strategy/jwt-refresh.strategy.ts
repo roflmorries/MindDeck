506d37c40db3c1a69fe3253df4f3e0f768183827
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-11-06 11:14:31 +0200

    feat(strategy) add jwt refresh strategy

--- mind-deck-backend/src/auth/strategy/jwt-refresh.strategy.ts
+++ mind-deck-backend/src/auth/strategy/jwt-refresh.strategy.ts
@@ -1,9 +1,11 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy, ExtractJwt } from 'passport-jwt'
+import { Strategy } from 'passport-custom'
 import { UserService } from "../../user/user.service";
 import { Request } from "express";
+import { JwtService } from "@nestjs/jwt";
+import { PrismaService } from "../../prisma.service";
 
 export interface JwtRefreshPayload {
   userId: string;
@@ -17,31 +19,55 @@ export interface JwtRefreshPayload {
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
   constructor(
-    private configService: ConfigService,
-    private userService: UserService
+    private config: ConfigService,
+    private userService: UserService,
+    private jwt: JwtService,
+    private prisma: PrismaService
   ) {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: configService.get('JWT_REFRESH_SECRET')!,
-      passReqToCallback: true,
-    })
+    super()
   }
 
-  async validate(req: Request, payload: JwtRefreshPayload) {
+  async validate(req: Request): Promise<any> {
 
-    const refreshToken = req.body?.refreshToken;
+    const refreshToken = req.cookies?.refreshToken;
 
     if (!refreshToken) {
-      throw new UnauthorizedException('Regresh token not found')
+      throw new UnauthorizedException('Refresh token not found')
     }
 
-    const user = this.authService;
+    let payload: JwtRefreshPayload;
+    try {
+      payload = this.jwt.verify(refreshToken, {
+        secret: this.config.get('JWT_REFRESH_SECRET')
+      })
+    } catch (error) {
+      throw new UnauthorizedException('Invalid refresh token signature')
+    }
 
+
+    const user = await this.userService.findUserById(payload.userId);
     if (!user) {
-      throw new UnauthorizedException('Invalid refresh token')
+      throw new UnauthorizedException('User not found')
     }
 
-    return user;
+    const isTokenValid = await this.prisma.refreshToken.findFirst({
+      where: {
+        token: refreshToken,
+        userId: user.id,
+        expiresAt: {gt: new Date()},
+      },
+    });
+
+    if (!isTokenValid) {
+      throw new UnauthorizedException('Invalid or expired refresh token');
+    }
+
+    return {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      refreshToken,
+      tokenId: isTokenValid.id
+    };
   }
 }
