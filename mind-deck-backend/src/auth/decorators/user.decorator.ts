691ff731d7c476d57165858af6e906575bb9b693
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-11-06 10:58:54 +0200

    feat(decorators) add more strict typing

--- mind-deck-backend/src/auth/decorators/user.decorator.ts
+++ mind-deck-backend/src/auth/decorators/user.decorator.ts
@@ -2,9 +2,13 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { User } from "@prisma/client";
 
 export const CurrentUser = createParamDecorator(
-  (data: keyof User, ctx: ExecutionContext) => {
+  (data: keyof User | undefined, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     const user = request.user;
+
+    if (!user) {
+      return null;
+    }
 
     return data ? user[data] : user;
   }
