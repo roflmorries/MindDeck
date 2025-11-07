7384e0937c80e9e94def7c2136a6262f5436ad0e
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-11-07 19:14:14 +0200

    fix(guard) fix typo

--- mind-deck-backend/src/auth/guards/jwt.guard.ts
+++ mind-deck-backend/src/auth/guards/jwt.guard.ts
@@ -1,9 +1,9 @@
-import { ExecutionContext } from "@nestjs/common";
+import { ExecutionContext, Injectable } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { AuthGuard } from "@nestjs/passport";
-import { Observable } from "rxjs";
 import { IS_PUBLIC_KEY } from "../decorators/public.decorator";
 
+@Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   constructor(private reflector: Reflector) {
     super();
