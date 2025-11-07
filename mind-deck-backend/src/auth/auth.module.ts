93c4a40ca81ae879e46e304df9ad9efddfd379d7
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-11-07 18:34:12 +0200

    refactor(auth) fix jwt conflict

--- mind-deck-backend/src/auth/auth.module.ts
+++ mind-deck-backend/src/auth/auth.module.ts
@@ -1,23 +1,47 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { UserModule } from '../user/user.module';
-import { JwtStrategy } from './strategy/jwt.strategy';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { JwtRefreshStrategy } from './strategy/jwt-refresh.strategy';
+// import { GoogleStrategy } from './strategy/google.strategy';
+
+import { UserModule } from '../user/user.module';
+import { PrismaService } from '../prisma.service';
 import { getJwtConfig } from '../config/jwt.config';
 
 @Module({
   imports: [
     UserModule,
     ConfigModule,
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: getJwtConfig
-    })
+    }),
   ],
+  
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  
+  providers: [
+    AuthService,
+    PrismaService,
+    JwtStrategy,
+    JwtRefreshStrategy,
+    // GoogleStrategy,
+  ],
+  
+  exports: [
+    AuthService,
+    JwtStrategy,
+    PassportModule,
+  ],
 })
 export class AuthModule {}
