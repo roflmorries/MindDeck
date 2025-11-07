87d41f29dcd80acb09afd42ae66206620010f453
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-11-07 18:33:34 +0200

    delete legacy code

--- mind-deck-backend/src/auth/decorators/role.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { Role } from '@prisma/client'
-
-export const ROLE_KEY = 'roles';
-export const Roles = (...roles: Role[]) => SetMetadata(ROLE_KEY, roles);
