e1562abbf091c809a215214cae762167d7c2e85d
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-11-07 18:22:06 +0200

    feat(user) update module

--- mind-deck-backend/src/user/user.module.ts
+++ mind-deck-backend/src/user/user.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from '../prisma.service';
+import { SocialAccountService } from './social-account.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, PrismaService],
-  exports: [UserService]
+  providers: [
+    UserService,
+    PrismaService,
+    SocialAccountService
+  ],
+  exports: [UserService, SocialAccountService]
 })
 export class UserModule {}
